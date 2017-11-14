6b6aa45a0dc5989b5a7bffe4f59a28368ba8db3d
HaoWang-TW <hawang@thoughtworks.com>
2017-11-14 21:27:43 +0800

    remove comment in webpack.development.config.js

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -18,8 +18,6 @@ module.exports = {
     ],
   },
   output: {
-    // path: path.resolve(__dirname, 'dist'),
-    // filename: './vendor.js',
     filename: '[name].js',
     path: path.join(__dirname, 'dist'),
     chunkFilename: '[name].js',
@@ -66,40 +64,28 @@ module.exports = {
     ],
   },
   plugins: [
-    // 定义环境变量为开发环境
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
       IS_DEVELOPMETN: true,
     }),
-    // 提取css
     new ExtractTextPlugin('vendor.[hash].css'),
-    // 根据入口文件，提取重复引用的公共代码类库，打包到单独文件中
-    // new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor', // 入口文件名
       filename: 'vendor.bundle.js', // 打包后的文件名
     }),
-    /* 压缩优化代码开始  可以关掉*/
-    // new webpack.optimize.UglifyJsPlugin({minimize: true}),
-    /* 压缩优化代码结束*/
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'app/index.html'),
     }),
     new OpenBrowserPlugin({
       url: `http://localhost:${PORT}/#/login`,
     }),
-    // 分析代码
-    // new BundleAnalyzerPlugin({ analyzerPort: 666 }),
   ],
   devtool: 'source-map',
   devServer: {
     contentBase: './app/',
     historyApiFallback: false,
     hot: false,
-    // inline: false,
-    // proxy: casProxy(),
     host: '0.0.0.0',
     port: PORT,
-    // stats: { colors: true },
   },
 }
